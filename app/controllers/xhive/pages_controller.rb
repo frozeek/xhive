2445da96169392be56ab17897aea0fd59a3e40b7
Marcelo Nieves <marcelo@frozeek.com>
2012-10-12 21:07:59 -0200

    Add pages controller

---
+++ app/controllers/xhive/pages_controller.rb
@@ -0,0 +1,24 @@
+require_dependency "xhive/application_controller"
+
+module Xhive
+  class PagesController < ApplicationController
+    extend Xhive::Widgify
+
+    widgify :widget
+
+    before_filter :load_page
+
+    def show
+    end
+
+    def widget
+      render :show
+    end
+
+  private
+
+    def load_page
+      @page = Page.find(params[:id])
+    end
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,2 +1,7 @@
 Xhive::Engine.routes.draw do
+  resources :pages, :only => :show do
+    get 'widget', :on => :member
+  end
+
+  root :to => 'pages#show'
 end
