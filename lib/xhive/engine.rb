0442d4dd3dc98101f0d5143a07429a66b6cc12fc
Marcelo Nieves <marcelo@frozeek.com>
2012-10-13 13:13:55 -0200

    Load all controllers on initialization

--- lib/xhive/engine.rb
+++ lib/xhive/engine.rb
@@ -1,5 +1,18 @@
+require "cells"
+
 module Xhive
   class Engine < ::Rails::Engine
     isolate_namespace Xhive
+    ::Cell::Base.prepend_view_path('app/cells')
+    initializer "xhive.extend_application_controller" do
+      ActiveSupport.on_load(:action_controller) do
+        extend Xhive::Widgify
+      end
+    end
+    initializer "xhive.load_all_controller_classes" do
+      ActiveSupport.on_load(:action_controller) do
+        Dir[Rails.root.join("app/controllers/**/*.rb")].each {|f| require f}
+      end
+    end
   end
 end
