e44870e48d15194b33dd624188f37bd5c85e474e
Marcelo Nieves <marcelo@frozeek.com>
2012-10-23 11:15:43 -0200

    Changes in initializers

--- lib/xhive/engine.rb
+++ lib/xhive/engine.rb
@@ -11,12 +11,13 @@ module Xhive
 
         include Xhive::ApplicationHelper
 
-        helper_method :initialize_widgets_loader
+        helper_method :initialize_widgets_loader, :include_custom_stylesheets
       end
     end
     initializer "xhive.load_all_controller_classes", :after=> :disable_dependency_loading do
       ActiveSupport.on_load(:action_controller) do
         Dir[Rails.root.join("app/controllers/**/*.rb")].each {|f| require f}
+        Xhive::Router::Cells.process_routes
       end
     end
   end
