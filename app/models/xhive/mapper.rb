ccb8a37d7150419c200564e6d49a30c35ab81f63
Marcelo Nieves <marcelo@frozeek.com>
2012-11-13 11:48:14 -0200

    Changes to Xhive::Mapper
    * Do not raise an error if mapper not found
    * Add documentation
    * Increase test coverage

--- app/models/xhive/mapper.rb
+++ app/models/xhive/mapper.rb
@@ -1,4 +1,6 @@
 module Xhive
+  # Maps resources to pages.
+  #
   class Mapper < ActiveRecord::Base
     attr_accessible :action, :page_id, :site_id, :resource, :key
 
@@ -10,13 +12,30 @@ module Xhive
     validates :site, :presence => true
     validates :page, :presence => true
 
+    # Public: looks for a page mapper and returns the associated page.
+    #
+    # site     - The Site to look into.
+    # resource - The String containing the resource name filter.
+    # action   - The String containing the action name filter.
+    # key      - The String containing the key filter.
+    #
+    # Returns: the mapped page or nil if not found.
+    #
     def self.page_for(site, resource, action, key = nil)
       mapper = find_map(site, resource, action, key)
       page = mapper.try(:page)
-      fail ActiveRecord::RecordNotFound unless page.present?
-      page
     end
 
+    # Public: creates a mapper for a specific resource and page.
+    #
+    # site     - The Site to associate the mapper to.
+    # page     - The Page object to map.
+    # resource - The String containing the associated resource name.
+    # action   - The String containing the associated action name.
+    # key      - The String containing the associated key.
+    #
+    # Returns: true if created. False otherwise.
+    #
     def self.map_resource(site, page, resource, action, key = nil)
       mapper = find_map(site, resource, action, key)
       mapper = new(:site_id => site.id, :resource => resource, :action => action, :key => key) unless mapper.present?
@@ -26,6 +45,15 @@ module Xhive
 
   private
 
+    # Private: looks for a mapper object.
+    #
+    # site     - The Site to look into.
+    # resource - The String containing the resource name filter.
+    # action   - The String containing the action name filter.
+    # key      - The String containing the key filter.
+    #
+    # Returns: the mapper object or nil if not found.
+    #
     def self.find_map(site, resource, action, key)
       mapper = where(:site_id => site.id).where(:resource => resource).where(:action => action)
       mapper = mapper.where(:key => key) if key.present?
--- test/unit/xhive/mapper_test.rb
+++ test/unit/xhive/mapper_test.rb
@@ -6,5 +6,41 @@ module Xhive
     should validate_presence_of(:page)
     should validate_presence_of(:resource)
     should validate_presence_of(:action)
+
+    setup do
+      @site = Site.create(:name => "default", :domain => 'localhost')
+      @page = @site.pages.create(:name => "default", :title => "Default Page", :content => "<h1>Hello World</h1>")
+      @mapper = @site.mappers.create(:resource => 'my_resource', :action => 'my_action', :key => 'my_key', :page_id => @page.id)
+    end
+
+    context 'page for' do
+      should 'return a valid page if mapper exists' do
+        assert_equal @page, Xhive::Mapper.page_for(@site, 'my_resource', 'my_action', 'my_key')
+      end
+
+      should 'return nil if mapper does not exist' do
+        assert_equal nil, Xhive::Mapper.page_for(@site, 'my_resource', 'my_action', 'my')
+      end
+    end
+
+    context 'map resource' do
+      should 'create a new mapper if it does not exist' do
+        Xhive::Mapper.map_resource(@site, @page, 'new_resource', 'my_action')
+
+        assert_equal 2, Xhive::Mapper.count
+        assert_equal @page, Xhive::Mapper.page_for(@site, 'new_resource', 'my_action')
+      end
+
+      should 'update the mapper if it already exists' do
+        assert_equal @page, Xhive::Mapper.page_for(@site, 'my_resource', 'my_action', 'my_key')
+
+        new_page = @site.pages.create(:name => "New Page", :title => "New Page", :content => "<h1>I am new</h1>")
+
+        Xhive::Mapper.map_resource(@site, new_page, 'my_resource', 'my_action', 'my_key')
+
+        assert_equal 1, Xhive::Mapper.count
+        assert_equal new_page, Xhive::Mapper.page_for(@site, 'my_resource', 'my_action', 'my_key')
+      end
+    end
   end
 end
