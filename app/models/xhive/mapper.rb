7e58a4c9396fb8d267b52a8084e37817dace39d7
Marcelo Nieves <marcelo@frozeek.com>
2012-11-14 16:13:43 -0200

    Changes to the mappers
    * Add all_by_resource class method
    * Now forcing to use the key in searches

--- app/models/xhive/mapper.rb
+++ app/models/xhive/mapper.rb
@@ -38,9 +38,21 @@ module Xhive
     #
     def self.map_resource(site, page, resource, action, key = nil)
       mapper = find_map(site, resource, action, key)
-      mapper = new(:site_id => site.id, :resource => resource, :action => action, :key => key) unless mapper.present?
+      mapper = new(:site_id => site.id, :resource => resource,
+                   :action => action, :key => key.present? ? key : nil) unless mapper.present?
       mapper.page = page
       mapper.save
+    end
+
+    # Public: returns all the mappers for a specific resource.
+    #
+    # site     - The Site owner of the mappers.
+    # resource - The String containing the resource to filter by.
+    #
+    # Returns: an ActiveRecord::Relation filtered by resource.
+    #
+    def self.all_by_resource(site, resource)
+      where(:site_id => site.id).where(:resource => resource)
     end
 
   private
@@ -55,8 +67,10 @@ module Xhive
     # Returns: the mapper object or nil if not found.
     #
     def self.find_map(site, resource, action, key)
-      mapper = where(:site_id => site.id).where(:resource => resource).where(:action => action)
-      mapper = mapper.where(:key => key) if key.present?
+      mapper = where(:site_id => site.id)
+      mapper = mapper.where(:resource => resource)
+      mapper = mapper.where(:action => action)
+      mapper = mapper.where(:key => key.present? ? key : nil)
       mapper.first
     end
   end
